f2433675ed3ffffd75ac3755b487997672d6a643
AlexanderDolgan <5704737@gmai.com>
2016-01-11 19:08:53 +0300

    last commit

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,11 +1,23 @@
+var myMap;
 
-//Trianglify background generator
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
 
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 10
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
 
-var pattern = Trianglify({
-    width: window.innerWidth,
-    height: 650,
-    cell_size: 40,
-    x_colors: 'random'
-});
-document.body.appendChild(pattern.canvas());
+    document.getElementById('destroyButton').onclick = function () {
+        // Для уничтожения используется метод destroy.
+        myMap.destroy();
+    };
+
+}
